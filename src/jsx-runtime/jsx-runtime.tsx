fec2ed3de400fb2f915fade3a5f480d9e271176e
reeli <reeli.null@gmail.com>
2021-11-01 18:59:46 +0800

    should forward ref to children

--- src/jsx-runtime/jsx-runtime.tsx
+++ src/jsx-runtime/jsx-runtime.tsx
@@ -1,11 +1,11 @@
-import { createContext, FC, useContext, ReactElement, cloneElement, Fragment as ReactFragment } from "react";
+import React, { FC, useContext, ReactElement } from "react";
 import { getConfig } from "../testid-config/config";
 
 const { runtime, testidKey, testidRoot } = getConfig();
 
 const combineIds = (ids: Array<string | undefined> = []) => ids.filter((v) => !!v).join(".");
 
-const TestidContext = createContext({
+const TestidContext = React.createContext({
   name: "",
 });
 
@@ -17,7 +17,7 @@ interface ConsumerProps {
   [key: string]: any;
 }
 
-const ConsumeTestid: FC<ConsumerProps> = ({ children, ...otherProps }) => {
+const ConsumeTestid: FC<ConsumerProps> = React.forwardRef(({ children, ...otherProps }, ref) => {
   const { name } = useContext(TestidContext);
 
   if (!children) {
@@ -27,22 +27,23 @@ const ConsumeTestid: FC<ConsumerProps> = ({ children, ...otherProps }) => {
   const nameOrRole = otherProps?.name || otherProps?.role;
   const testidOrNameOrRole = otherProps[testidRoot] || nameOrRole;
   const id = combineIds([name, nameOrRole]);
-  const propsToChildren = id && !otherProps[testidKey] ? { ...otherProps, [testidKey]: id } : otherProps;
+  const propsToChildren =
+    id && !otherProps[testidKey] ? { ...otherProps, [testidKey]: id, ref } : { ...otherProps, ref };
 
   if (testidOrNameOrRole) {
     const newName = combineIds([name, testidOrNameOrRole]);
 
     return (
       <TestidContext.Provider value={{ name: newName }}>
-        {cloneElement(children, propsToChildren)}
+        {React.cloneElement(children, propsToChildren)}
       </TestidContext.Provider>
     );
   }
 
-  return cloneElement(children, propsToChildren);
-};
+  return React.cloneElement(children, propsToChildren);
+});
 
-export const Fragment = ReactFragment;
+export const Fragment = React.Fragment;
 
 export const jsx = (...args: Parameters<typeof runtime.jsx>) => {
   const [type, props, key] = args;
